608e55d901abc7fe886313fe75ea9a0d473c9475
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2023-12-16 14:27:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function playVideo(){
+    const videoContainer = document.getElementById("video-container");
+    const youtubeFrame = document.createElement('div');
+
+    youtubeFrame.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/cScUz023JtQ?si=oMGC8PzmzYb1MfZl&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    videoContainer.appendChild(youtubeFrame);
+    videoContainer.style.visibility = "visible"
+}
 const emojis=["😢","😢","🙂","🙂","👹","👹","😍","😍","❤️","❤️","💪","💪","🔥","🔥","🎃","🎃"];
         var shuf_emojis=emojis.sort(() =>(Math.random()>.5)?2:-1);
         for(var i=0;i<emojis.length;i++){
@@ -10,6 +18,7 @@ const emojis=["😢","😢","🙂","🙂","👹","👹","😍","😍","❤️","
                  }
               function closepopup(){
               popup.classList.remove("open-popup");
+              playVideo();
               }
                               
             box.onclick=function(){
